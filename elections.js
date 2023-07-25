6aab80727e418555730187df1969c701e0b3bf79
fcegatti <fcegatti@yahoo.es>
2023-07-25 02:57:20 +0200

    Add basic elections data and route

---
+++ elections.js
@@ -0,0 +1,226 @@
+// Datos de muestra para las elecciones
+const elections = [
+  {
+    id: 1,
+    type: 'Generales',
+    seats: 350,
+    parties: ['Partido A', 'Partido B', 'Partido C', 'Partidp D'],
+    provinces: [
+      {
+        name: 'Madrid',
+        seats: 37,
+      },
+      {
+        name: 'Barcelona',
+        seats: 32,
+      },
+      {
+        name: 'Valencia',
+        seats: 16,
+      },
+      {
+        name: 'Alicante',
+        seats: 12,
+      },
+      {
+        name: 'Sevilla',
+        seats: 12,
+      },
+      {
+        name: 'Málaga',
+        seats: 11,
+      },
+      {
+        name: 'Murcia',
+        seats: 10,
+      },
+      {
+        name: 'Cádiz',
+        seats: 9,
+      },
+      {
+        name: 'A Coruña',
+        seats: 8,
+      },
+      {
+        name: 'Illes Balears',
+        seats: 8,
+      },
+      {
+        name: 'Las Palmas',
+        seats: 8,
+      },
+      {
+        name: 'Vizcaya',
+        seats: 8,
+      },
+      {
+        name: 'Pontevedra',
+        seats: 7,
+      },
+      {
+        name: 'Santa Cruz de Tenerife',
+        seats: 7,
+      },
+      {
+        name: 'Asturias',
+        seats: 7,
+      },
+      {
+        name: 'Granada',
+        seats: 7,
+      },
+      {
+        name: 'Zaragoza',
+        seats: 7,
+      },
+      {
+        name: 'Guipúzcoa',
+        seats: 6,
+      },
+      {
+        name: 'Tarragona',
+        seats: 6,
+      },
+      {
+        name: 'Girona',
+        seats: 6,
+      },
+      {
+        name: 'Alemría',
+        seats: 6,
+      },
+      {
+        name: 'Toledo',
+        seats: 6,
+      },
+      {
+        name: 'Córdoba',
+        seats: 6,
+      },
+      {
+        name: 'Huelva',
+        seats: 5,
+      },
+      {
+        name: 'Valladolid',
+        seats: 5,
+      },
+      {
+        name: 'Cantabria',
+        seats: 5,
+      },
+      {
+        name: 'Ciudad Real',
+        seats: 5,
+      },
+      {
+        name: 'Navarra',
+        seats: 5,
+      },
+      {
+        name: 'Castellón',
+        seats: 5,
+      },
+      {
+        name: 'Jaén',
+        seats: 5,
+      },
+      {
+        name: 'Badajoz',
+        seats: 5,
+      },
+      {
+        name: 'Lugo',
+        seats: 4,
+      },
+      {
+        name: 'Lleida',
+        seats: 4,
+      },
+      {
+        name: 'Burgos',
+        seats: 4,
+      },
+      {
+        name: 'León',
+        seats: 4,
+      },
+      {
+        name: 'Albacete',
+        seats: 4,
+      },
+      {
+        name: 'La Rioja',
+        seats: 4,
+      },
+      {
+        name: 'Cáceres',
+        seats: 4,
+      },
+      {
+        name: 'Ourense',
+        seats: 4,
+      },
+      {
+        name: 'Salamanca',
+        seats: 4,
+      },
+      {
+        name: 'Álava',
+        seats: 4,
+      },
+      {
+        name: 'Zamora',
+        seats: 3,
+      },
+      {
+        name: 'Guadalajara',
+        seats: 3,
+      },
+      {
+        name: 'Segovia',
+        seats: 3,
+      },
+      {
+        name: 'Teruel',
+        seats: 3,
+      },
+      {
+        name: 'Huesca',
+        seats: 3,
+      },
+      {
+        name: 'Cuenca',
+        seats: 3,
+      },
+      {
+        name: 'Ávila',
+        seats: 3,
+      },
+      {
+        name: 'palencia',
+        seats: 3,
+      },
+      {
+        name: 'Soria',
+        seats: 2,
+      },
+      {
+        name: 'melilla',
+        seats: 1,
+      },
+      {
+        name: 'Ceuta',
+        seats: 1,
+      },
+
+      
+
+      
+    ],
+  },
+  
+];
+
+module.exports = elections;
--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const elections = require('./elections');
 
 app.get('/', (req, res) => {
   res.send('Te calculo los escaños');
+});
+
+app.get('/election/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const election = elections.find(e => e.id === id);
+  if (election) {
+    res.json(election);
+  } else {
+    res.status(404).json({message: 'Election not found'});
+  }
 });
 
 app.listen(port, () => {
