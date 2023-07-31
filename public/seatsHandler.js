1dd480be4d84aca2750d9f8d7d151097ef58c7d0
fcegatti <fcegatti@yahoo.es>
2023-07-31 04:39:01 +0200

    Add title total seats count at state-level and default value

--- public/seatsHandler.js
+++ public/seatsHandler.js
@@ -1,31 +1,46 @@
 document.addEventListener('DOMContentLoaded', function() {
   console.log('DOM fully loaded and parsed');
-
+  
+  const electionTypeSelect = document.querySelector('#electionType');
+  const chamberSelect = document.querySelector('#chamber');
   const acSelect = document.querySelector('#autonomousCommunity');
   const provinceSelect = document.querySelector('#province');
   const seatsSectionTitle = document.querySelector('.seats-graph h2');
 
   function updateSeatsSectionTitle() {
+    const electionType = electionTypeSelect.value;
+    const chamber = chamberSelect.value;
     const acName = acSelect.value;
-    const provinceName = provinceSelect.value;
+    let provinceName = provinceSelect.value;
 
     if (!acName) {
-      console.error('acName is not defined');
+      if (electionType === 'generales' && chamber === 'congreso') {
+        seatsSectionTitle.textContent = `ESCAÑOS (350)`;
+      } else {
+        seatsSectionTitle.textContent = `Escaños (350)`;
+      }
       return;
     }
+
+    
 
     const url = provinceName
       ? `/api/seats/${acName}/${provinceName}`
       : `/api/seats/${acName}`;
 
-    fetch(`/api/seats/${acName}/${provinceName}`)
+    fetch(url)
       .then(response => response.json())
-      .then(data => {seatsSectionTitle.textContent = `Escaños (${data.totalSeats})`;
+      .then(data => {seatsSectionTitle.textContent = `ESCAÑOS (${data.totalSeats})`;
       })
       .catch(error => console.error('Error', error))
   }
 
-  acSelect.addEventListener('change', updateSeatsSectionTitle);
+  electionTypeSelect.addEventListener('change', updateSeatsSectionTitle);
+  chamberSelect.addEventListener('change', updateSeatsSectionTitle);
+  acSelect.addEventListener('change', function () {
+    provinceSelect.selectedIndex = 0; // Reseteo la selección de la provincia cuando se selecciona una comunidad autónoma para que no devuelva undefined al volver a la CCAA
+    updateSeatsSectionTitle();
+  });
   provinceSelect.addEventListener('change', updateSeatsSectionTitle);
 
   updateSeatsSectionTitle();
