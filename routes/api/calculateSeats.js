6c0179f29e0ca50e49bb04f14129296f0e766857
fcegatti <fcegatti@yahoo.es>
2023-08-09 19:56:32 +0200

    Implement data persistence by writing seat calculations to electionsData.json

--- routes/api/calculateSeats.js
+++ routes/api/calculateSeats.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const electionsData = require('../../data/electionsData.json');
 const calculateSeats = require('../../services/calculateSeats');
+const fs = require('fs');
+const path = require('path');
 
 router.post('/', (req, res) => {
   // Extraer los datos del formulario del objeto req.body
@@ -13,7 +16,18 @@ router.post('/', (req, res) => {
   try {
     // Calcular los escaños
     const seatResults = calculateSeats(votesData);
+
+    const acData = electionsData.autonomousCommunities.find(ac => ac.name === votesData.community);
+
+    if (!acData) {
+      return res.status(400).json({ message: 'Invalid community provided'});
+    }
+
+    acData.parties = seatResults;
+    fs.writeFileSync(path.join(__dirname, '../../data/electionsData.json'), JSON.stringify(electionsData, null, 2));
+
     res.json(seatResults);
+
   } catch (error) {
     console.error('Error', error);
     res.status(500).json({message: 'An error occurred while calculating seats'})
